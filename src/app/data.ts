3ec7d86de90104b30f26f2b929a6215fe9648fc7
Franco Leonardo Bulgarelli <franco@mumuki.org>
2023-07-07 01:56:17 -0300

    Initial version

---
+++ src/app/data.ts
@@ -0,0 +1,501 @@
+
+const presente = [
+  {
+    'verbo': 'caber',
+    'eu': 'caibo',
+    'ele': 'cabe',
+    'nós': 'cabemos',
+    'eles': 'cabem'
+  },
+  {
+    'verbo': 'cair',
+    'eu': 'caio',
+    'ele': 'cai',
+    'nós': 'caímos',
+    'eles': 'caem'
+  },
+  {
+    'verbo': 'cobrir',
+    'eu': 'cubro',
+    'ele': 'cobre',
+    'nós': 'cobrimos',
+    'eles': 'cobrem'
+  },
+  {
+    'verbo': 'construir',
+    'eu': 'construo',
+    'ele': 'constrói',
+    'nós': 'construímos',
+    'eles': 'constroem'
+  },
+  { 'verbo': 'dar', 'eu': 'dou', 'ele': 'dá', 'nós': 'damos', 'eles': 'dão' },
+  {
+    'verbo': 'descobrir',
+    'eu': 'descubro',
+    'ele': 'descobre',
+    'nós': 'descobrimos',
+    'eles': 'descobrem'
+  },
+  {
+    'verbo': 'destruir',
+    'eu': 'destruo',
+    'ele': 'destrói',
+    'nós': 'destruímos',
+    'eles': 'destroem'
+  },
+  {
+    'verbo': 'distribuir',
+    'eu': 'distribuo',
+    'ele': 'distribui',
+    'nós': 'distribuímos',
+    'eles': 'distribuem'
+  },
+  {
+    'verbo': 'dizer',
+    'eu': 'digo',
+    'ele': 'diz',
+    'nós': 'dizemos',
+    'eles': 'dizem'
+  },
+  {
+    'verbo': 'dormir',
+    'eu': 'durmo',
+    'ele': 'dorme',
+    'nós': 'dormimos',
+    'eles': 'dormem'
+  },
+  {
+    'verbo': 'estar',
+    'eu': 'estou',
+    'ele': 'está',
+    'nós': 'estamos',
+    'eles': 'estão'
+  },
+  {
+    'verbo': 'fazer',
+    'eu': 'faço',
+    'ele': 'faz',
+    'nós': 'fazemos',
+    'eles': 'fazem'
+  },
+  {
+    'verbo': 'fugir',
+    'eu': 'fujo',
+    'ele': 'foge',
+    'nós': 'fugimos',
+    'eles': 'fogem'
+  },
+  { 'verbo': 'haver', 'eu': 'hei', 'ele': 'há', 'nós': 'hemos', 'eles': 'hão' },
+  { 'verbo': 'ir', 'eu': 'vou', 'ele': 'vai', 'nós': 'vamos', 'eles': 'vão' },
+  { 'verbo': 'ler', 'eu': 'leio', 'ele': 'lê', 'nós': 'lemos', 'eles': 'leem' },
+  {
+    'verbo': 'medir',
+    'eu': 'meço',
+    'ele': 'mede',
+    'nós': 'medimos',
+    'eles': 'medem'
+  },
+  {
+    'verbo': 'odiar',
+    'eu': 'odeio',
+    'ele': 'odeia',
+    'nós': 'odiamos',
+    'eles': 'odeiam'
+  },
+  {
+    'verbo': 'ouvir',
+    'eu': 'ouço',
+    'ele': 'ouve',
+    'nós': 'ouvimos',
+    'eles': 'ouvem'
+  },
+  {
+    'verbo': 'passear',
+    'eu': 'passeio',
+    'ele': 'passeia',
+    'nós': 'passeamos',
+    'eles': 'passeiam'
+  },
+  {
+    'verbo': 'pedir',
+    'eu': 'peço',
+    'ele': 'pede',
+    'nós': 'pedimos',
+    'eles': 'pedem'
+  },
+  {
+    'verbo': 'perder',
+    'eu': 'perco',
+    'ele': 'perde',
+    'nós': 'perdemos',
+    'eles': 'perdem'
+  },
+  {
+    'verbo': 'poder',
+    'eu': 'posso',
+    'ele': 'pode',
+    'nós': 'podemos',
+    'eles': 'podem'
+  },
+  {
+    'verbo': 'pôr',
+    'eu': 'ponho',
+    'ele': 'põe',
+    'nós': 'pomos',
+    'eles': 'põem'
+  },
+  {
+    'verbo': 'preferir',
+    'eu': 'prefiro',
+    'ele': 'prefere',
+    'nós': 'preferimos',
+    'eles': 'preferem'
+  },
+  {
+    'verbo': 'querer',
+    'eu': 'quero',
+    'ele': 'quer',
+    'nós': 'queremos',
+    'eles': 'querem'
+  },
+  {
+    'verbo': 'repetir',
+    'eu': 'repito',
+    'ele': 'repete',
+    'nós': 'repetimos',
+    'eles': 'repetem'
+  },
+  { 'verbo': 'rir', 'eu': 'rio', 'ele': 'ri', 'nós': 'rimos', 'eles': 'riem' },
+  {
+    'verbo': 'saber',
+    'eu': 'sei',
+    'ele': 'sabe',
+    'nós': 'sabemos',
+    'eles': 'sabem'
+  },
+  {
+    'verbo': 'sair',
+    'eu': 'saio',
+    'ele': 'sai',
+    'nós': 'saímos',
+    'eles': 'saem'
+  },
+  {
+    'verbo': 'sentir',
+    'eu': 'sinto',
+    'ele': 'sente',
+    'nós': 'sentimos',
+    'eles': 'sentem'
+  },
+  { 'verbo': 'ser', 'eu': 'sou', 'ele': 'é', 'nós': 'somos', 'eles': 'são' },
+  {
+    'verbo': 'sorrir',
+    'eu': 'sorrio',
+    'ele': 'sorri',
+    'nós': 'sorrimos',
+    'eles': 'sorriem'
+  },
+  {
+    'verbo': 'subir',
+    'eu': 'subo',
+    'ele': 'sobe',
+    'nós': 'subimos',
+    'eles': 'sobem'
+  },
+  {
+    'verbo': 'substituir',
+    'eu': 'substituo',
+    'ele': 'substitui',
+    'nós': 'substituímos',
+    'eles': 'substituem'
+  },
+  { 'verbo': 'ter', 'eu': 'tenho', 'ele': 'tem', 'nós': 'temos', 'eles': 'têm' },
+  {
+    'verbo': 'trazer',
+    'eu': 'trago',
+    'ele': 'traz',
+    'nós': 'trazemos',
+    'eles': 'trazem'
+  },
+  {
+    'verbo': 'valer',
+    'eu': 'valho',
+    'ele': 'vale',
+    'nós': 'valemos',
+    'eles': 'valem'
+  },
+  { 'verbo': 'ver', 'eu': 'vejo', 'ele': 'vê', 'nós': 'vemos', 'eles': 'veem' },
+  {
+    'verbo': 'vestir',
+    'eu': 'visto',
+    'ele': 'veste',
+    'nós': 'vestimos',
+    'eles': 'vestem'
+  },
+  { 'verbo': 'vir', 'eu': 'venho', 'ele': 'vem', 'nós': 'vimos', 'eles': 'vêm' }]
+
+const pasado = [
+  {
+    'verbo': 'caber',
+    'eu': 'coube',
+    'ele': 'coube',
+    'nós': 'coubemos',
+    'eles': 'couberam'
+  },
+  {
+    'verbo': 'cair',
+    'eu': 'caí',
+    'ele': 'caiu',
+    'nós': 'caímos',
+    'eles': 'caíram'
+  },
+  {
+    'verbo': 'cobrir',
+    'eu': 'cobri',
+    'ele': 'cobriu',
+    'nós': 'cobrimos',
+    'eles': 'cobriram'
+  },
+  {
+    'verbo': 'construir',
+    'eu': 'construí',
+    'ele': 'construiu',
+    'nós': 'construímos',
+    'eles': 'construíram'
+  },
+  { 'verbo': 'dar', 'eu': 'dei', 'ele': 'deu', 'nós': 'demos', 'eles': 'deram' },
+  {
+    'verbo': 'descobrir',
+    'eu': 'descobri',
+    'ele': 'descobriu',
+    'nós': 'descobrimos',
+    'eles': 'descobriram'
+  },
+  {
+    'verbo': 'destruir',
+    'eu': 'destruí',
+    'ele': 'destruiu',
+    'nós': 'destruímos',
+    'eles': 'destruíram'
+  },
+  {
+    'verbo': 'distribuir',
+    'eu': 'distribuí',
+    'ele': 'distribuiu',
+    'nós': 'distribuímos',
+    'eles': 'distribuíram'
+  },
+  {
+    'verbo': 'dizer',
+    'eu': 'disse',
+    'ele': 'disse',
+    'nós': 'dissemos',
+    'eles': 'disseram'
+  },
+  {
+    'verbo': 'dormir',
+    'eu': 'dormi',
+    'ele': 'dormiu',
+    'nós': 'dormimos',
+    'eles': 'dormiram'
+  },
+  {
+    'verbo': 'estar',
+    'eu': 'estive',
+    'ele': 'esteve',
+    'nós': 'estivemos',
+    'eles': 'estiveram'
+  },
+  {
+    'verbo': 'fazer',
+    'eu': 'fiz',
+    'ele': 'fez',
+    'nós': 'fizemos',
+    'eles': 'fizeram'
+  },
+  {
+    'verbo': 'fugir',
+    'eu': 'fugi',
+    'ele': 'fugiu',
+    'nós': 'fugimos',
+    'eles': 'fugiram'
+  },
+  {
+    'verbo': 'haver',
+    'eu': 'houve',
+    'ele': 'houve',
+    'nós': 'houvemos',
+    'eles': 'houveram'
+  },
+  { 'verbo': 'ir', 'eu': 'fui', 'ele': 'foi', 'nós': 'fomos', 'eles': 'foram' },
+  { 'verbo': 'ler', 'eu': 'li', 'ele': 'leu', 'nós': 'lemos', 'eles': 'leram' },
+  {
+    'verbo': 'medir',
+    'eu': 'medi',
+    'ele': 'mediu',
+    'nós': 'medimos',
+    'eles': 'mediram'
+  },
+  {
+    'verbo': 'odiar',
+    'eu': 'odiei',
+    'ele': 'odiou',
+    'nós': 'odiamos',
+    'eles': 'odiaram'
+  },
+  {
+    'verbo': 'ouvir',
+    'eu': 'ouvi',
+    'ele': 'ouviu',
+    'nós': 'ouvimos',
+    'eles': 'ouviram'
+  },
+  {
+    'verbo': 'passear',
+    'eu': 'passeei',
+    'ele': 'passeou',
+    'nós': 'passeamos',
+    'eles': 'passearam'
+  },
+  {
+    'verbo': 'pedir',
+    'eu': 'pedi',
+    'ele': 'pediu',
+    'nós': 'pedimos',
+    'eles': 'pediram'
+  },
+  {
+    'verbo': 'perder',
+    'eu': 'perdi',
+    'ele': 'perdeu',
+    'nós': 'perdemos',
+    'eles': 'perderam'
+  },
+  {
+    'verbo': 'poder',
+    'eu': 'pude',
+    'ele': 'pôde',
+    'nós': 'pudemos',
+    'eles': 'puderam'
+  },
+  {
+    'verbo': 'pôr',
+    'eu': 'pus',
+    'ele': 'pôs',
+    'nós': 'pusemos',
+    'eles': 'puseram'
+  },
+  {
+    'verbo': 'preferir',
+    'eu': 'preferi',
+    'ele': 'preferiu',
+    'nós': 'preferimos',
+    'eles': 'preferiram'
+  },
+  {
+    'verbo': 'querer',
+    'eu': 'quis',
+    'ele': 'quis',
+    'nós': 'quisemos',
+    'eles': 'quiseram'
+  },
+  {
+    'verbo': 'repetir',
+    'eu': 'repeti',
+    'ele': 'repetiu',
+    'nós': 'repetimos',
+    'eles': 'repetiram'
+  },
+  { 'verbo': 'rir', 'eu': 'ri', 'ele': 'riu', 'nós': 'rimos', 'eles': 'riram' },
+  {
+    'verbo': 'saber',
+    'eu': 'soube',
+    'ele': 'soube',
+    'nós': 'soubemos',
+    'eles': 'souberam'
+  },
+  {
+    'verbo': 'sair',
+    'eu': 'saí',
+    'ele': 'saiu',
+    'nós': 'saímos',
+    'eles': 'saíram'
+  },
+  {
+    'verbo': 'sentir',
+    'eu': 'senti',
+    'ele': 'sentiu',
+    'nós': 'sentimos',
+    'eles': 'sentiram'
+  },
+  { 'verbo': 'ser', 'eu': 'fui', 'ele': 'foi', 'nós': 'fomos', 'eles': 'foram' },
+  {
+    'verbo': 'sorrir',
+    'eu': 'sorri',
+    'ele': 'sorriu',
+    'nós': 'sorrimos',
+    'eles': 'sorriram'
+  },
+  {
+    'verbo': 'subir',
+    'eu': 'subi',
+    'ele': 'subiu',
+    'nós': 'subimos',
+    'eles': 'subiram'
+  },
+  {
+    'verbo': 'substituir',
+    'eu': 'substituí',
+    'ele': 'substituiu',
+    'nós': 'substituímos',
+    'eles': 'substituíram'
+  },
+  {
+    'verbo': 'ter',
+    'eu': 'tive',
+    'ele': 'teve',
+    'nós': 'tivemos',
+    'eles': 'tiveram'
+  },
+  {
+    'verbo': 'trazer',
+    'eu': 'trouxe',
+    'ele': 'trouxe',
+    'nós': 'trouxemos',
+    'eles': 'trouxeram'
+  },
+  {
+    'verbo': 'valer',
+    'eu': 'vali',
+    'ele': 'valeu',
+    'nós': 'valemos',
+    'eles': 'valeram'
+  },
+  { 'verbo': 'ver', 'eu': 'vi', 'ele': 'viu', 'nós': 'vimos', 'eles': 'viram' },
+  {
+    'verbo': 'vestir',
+    'eu': 'vesti',
+    'ele': 'vestiu',
+    'nós': 'vestimos',
+    'eles': 'vestiram'
+  },
+  {
+    'verbo': 'vir',
+    'eu': 'vim',
+    'ele': 'veio',
+    'nós': 'viemos',
+    'eles': 'vieram'
+  }];
+
+function generateCards(verbs: any[], category: string) {
+  return verbs.flatMap(it => [
+    { item: it.verbo, question: "eu _", answer: "eu " + it.eu },
+    { item: it.verbo, question: "ele _", answer: "ele " + it.ele },
+    { item: it.verbo, question: "eles _", answer: "eles " + it.eles },
+    { item: it.verbo, question: "nós _", answer: "nós " + it['nós'] },
+  ]).map(it => ({ ...it, category: category }))
+}
+
+const cards =  generateCards(presente, 'Presente').concat(generateCards(pasado, 'Pretérito Perfeito'))
+
+export { cards };
